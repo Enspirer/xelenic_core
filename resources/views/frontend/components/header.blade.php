a56dfdc11fa46d898f6490225b3f901fe8db7e27
Anupama-Dikkubura <anupamadikkubura@gmail.com>
2020-03-14 13:28:00 +0530

    Pages blade created

--- resources/views/frontend/components/header.blade.php
+++ resources/views/frontend/components/header.blade.php
@@ -1,5 +1,5 @@
 <header id="header" id="home">
-    
+
     <div class="container main-menu">
         <div class="row align-items-center justify-content-between d-flex">
             <div id="logo">
@@ -7,18 +7,14 @@
             </div>
             <nav id="nav-menu-container">
                 <ul class="nav-menu">
-                    <li class="menu-active"><a href="#home">Home</a></li>
-                    <li><a href="#plan">Plan</a></li>
-                    <li><a href="#about">About</a></li>
-                    <li><a href="#service">Service</a></li>
-                    <li><a href="#faq">Faq</a></li>
-                    <li><a href="#contact">Contact</a></li>
-                    <li class="menu-has-children"><a href="">Pages</a>
-                        <ul>
-                            <li><a href="generic.html">Generic</a></li>
-                            <li><a href="elements.html">Elements</a></li>
-                        </ul>
-                    </li>
+                    <li class="menu-active"><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/services">Service</a></li>
+                    <li><a href="/products">Products</a></li>
+                    <li><a href="/partnerships">Partnerships</a></li>
+                    <li><a href="/blog">Blog</a></li>
+                    <li><a href="/faq">Faq</a></li>
+                    <li><a href="/contact">Contact</a></li>
                 </ul>
             </nav><!-- #nav-menu-container -->
         </div>
