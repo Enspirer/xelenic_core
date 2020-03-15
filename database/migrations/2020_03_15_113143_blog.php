48cd09dc4bc17bc4c67725229677701b6a6752f6
Sanjaya Harshana <sanjayaharasana@gmail.com>
2020-03-15 17:05:39 +0530

    migration blog create

---
+++ database/migrations/2020_03_15_113143_blog.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Blog extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('blog_table', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->uuid('blog_title');
+            $table->longText('body')->nullable();
+            $table->string('thums_img');
+            $table->string('slug');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('blog_table');
+    }
+}
