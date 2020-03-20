38bd468aa5072a70261288a182eae8a70f439aa0
Anupama Dikkumbura <anupamadikkubura@gmail.com>
2020-03-20 08:24:29 +0530

    Team controller and route created

--- routes/frontend/home.php
+++ routes/frontend/home.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\PartnershipsController;
 use App\Http\Controllers\BlogController;
 use App\Http\Controllers\FaqController;
+use App\Http\Controllers\TeamController;
+
 use App\Http\Controllers\Frontend\ContactController;
 use App\Http\Controllers\Frontend\HomeController;
 use App\Http\Controllers\Frontend\User\AccountController;
@@ -24,6 +26,7 @@ Route::get('partnerships', [PartnershipsController::class, 'index'])->name('inde
 Route::get('blog', [BlogController::class, 'index'])->name('index');
 Route::get('faq', [FaqController::class, 'index'])->name('index');
 Route::get('contact', [ContactController::class, 'index'])->name('contact');
+Route::get('team', [TeamController::class, 'index'])->name('index');
 
 Route::post('contact/send', [ContactController::class, 'send'])->name('contact.send');
 
