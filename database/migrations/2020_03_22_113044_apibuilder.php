81e69fa8f9bccaeab1cec6a5f847fd5130ba0f6f
Sanjaya Harshana <sanjayaharasana@gmail.com>
2020-03-23 17:11:27 +0530

    API Builder Migration

--- database/migrations/2020_03_22_113044_apibuilder.php
+++ database/migrations/2020_03_22_113044_apibuilder.php
@@ -18,8 +18,30 @@ class Apibuilder extends Migration
             $table->text('app_name');
             $table->integer('user_id');
             $table->integer('app_type');
-            $table->integer('tokeing');
-            $table->integer('intence_count');
+            $table->string('status');
+            $table->text('key');
+            $table->timestamp('updated_at');
+            $table->timestamp('created_at')->useCurrent();
+        });
+
+        Schema::create('cloud_api_data_table', function (Blueprint $table) {
+            $table->bigIncrements('table_id');
+            $table->text('table_name');
+            $table->integer('user_id');
+            $table->text('key');
+            $table->integer('ab_id');
+            $table->integer('status');
+            $table->timestamp('updated_at');
+            $table->timestamp('created_at')->useCurrent();
+        });
+
+        Schema::create('cloud_api_data_field', function (Blueprint $table) {
+            $table->bigIncrements('data_field_id');
+            $table->text('field_name');
+            $table->text('type');
+            $table->text('key');
+            $table->integer('ab_id');
+            $table->integer('status');
             $table->timestamp('updated_at');
             $table->timestamp('created_at')->useCurrent();
         });
