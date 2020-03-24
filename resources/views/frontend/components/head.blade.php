94fc1d2dd603c1da1d6ab98f2d24abbfaee723be
Anupama Dikkumbura <anupamadikkubura@gmail.com>
2020-03-24 21:16:52 +0530

    about page updated

--- resources/views/frontend/components/head.blade.php
+++ resources/views/frontend/components/head.blade.php
@@ -29,4 +29,9 @@
     <link rel="stylesheet" href="FrontPage/css/main.css">
     <script src="FrontPage/js/vendor/jquery-2.2.4.min.js"></script>
 
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
 
