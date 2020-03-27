9bac1e386c3acc7c16cf766bb2577b6ff5ce07f6
Sanjaya Harshana <sanjayaharasana@gmail.com>
2020-03-27 16:16:56 +0530

    Cloud API Data Table

--- app/ServiceModel/CloudAPIBuilder/CloudAPIDataTable.php
+++ app/ServiceModel/CloudAPIBuilder/CloudAPIDataTable.php
@@ -3,8 +3,49 @@
 namespace App\ServiceModel\CloudAPIBuilder;
 
 use Illuminate\Database\Eloquent\Model;
+use DB;
+use App\Http\Controllers\CommonFunctions\Entrosement;
 
 class CloudAPIDataTable extends Model
 {
-    //
+    protected $table = 'cloud_api_data_table';
+    protected $primaryKey = 'table_id';
+
+    public static function create_table($table_name,$ab_id,$status)
+    {
+        $key = Entrosement::generate_APIKey(30);
+
+        $id = DB::table('cloud_api_data_table')->insertGetId(
+            [
+                'table_name' => $table_name,
+                'user_id' => auth()->user()->id,
+                'key' => $key,
+                'ab_id' => $ab_id,
+                'status' => $status,
+            ]
+        );
+
+        return $id;
+    }
+
+
+
+    public static function get_table_by_id ($table_id)
+    {
+        $get_table = DB::table('cloud_api_data_table')
+            ->where('table_id',$table_id)
+            ->first();
+        return $get_table;
+    }
+
+    public static function get_tables_by_user ($user_id)
+    {
+        $get_table = DB::table('cloud_api_data_table')
+            ->where('user_id',$user_id)
+            ->get();
+
+        return $get_table;
+    }
+
+
 }
