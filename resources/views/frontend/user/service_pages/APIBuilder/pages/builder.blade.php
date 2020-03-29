faba47cf001a1462f02939d7c99ae2392b3852ce
Sanjaya Harshana <sanjayaharasana@gmail.com>
2020-03-30 02:15:57 +0530

    Data filed Creator Blade

--- resources/views/frontend/user/service_pages/APIBuilder/pages/builder.blade.php
+++ resources/views/frontend/user/service_pages/APIBuilder/pages/builder.blade.php
@@ -32,10 +32,10 @@
             <div class="container-fluid px-xxl-65 px-xl-20">
                 <ul class="nav nav-light nav-tabs" role="tablist">
                     <li class="nav-item">
-                        <a data-toggle="tab" href="#builder_dashboard" class="d-flex h-60p align-items-center nav-link">Dashboard</a>
+                        <a data-toggle="tab" onclick="location_changer('builder_dashboard')" href="#builder_dashboard" class="d-flex h-60p align-items-center nav-link">Dashboard</a>
                     </li>
                     <li class="nav-item">
-                        <a data-toggle="tab" href="#builder_table" class="d-flex h-60p align-items-center nav-link">Tables</a>
+                        <a data-toggle="tab"  onclick="location_changer('builder_table')" href="#builder_table" class="d-flex h-60p align-items-center nav-link">Tables</a>
                     </li>
                     <li class="nav-item">
                         <a data-toggle="tab" href="#builder_api_logs" class="d-flex h-60p align-items-center nav-link">API Logs</a>
@@ -65,6 +65,13 @@
 
 
 @include('frontend.user.dashboard_components.footer')
+
+    <script>
+        function location_changer(hash_code) {
+            location.hash = hash_code;
+        }
+
+    </script>
 
     <script>
         $(function(){
---
+++ resources/views/frontend/user/service_pages/APIBuilder/pages/phaster_views/data_fied_creator.blade.php
@@ -0,0 +1,102 @@
+@include('frontend.user.dashboard_components.header')
+
+<!-- Top Navbar -->
+@include('frontend.user.dashboard_components.top_navbar')
+@include('frontend.user.dashboard_components.search_box')
+
+<!-- /Top Navbar -->
+
+<!-- Vertical Nav -->
+@include('frontend.user.dashboard_components.sidebar')
+
+<div id="hk_nav_backdrop" class="hk-nav-backdrop"></div>
+<!-- /Vertical Nav -->
+
+
+<!-- Main Content -->
+<div class="hk-pg-wrapper">
+    <!-- Breadcrumb -->
+    <nav class="hk-breadcrumb" aria-label="breadcrumb">
+        <ol class="breadcrumb breadcrumb-light bg-transparent">
+            <li class="breadcrumb-item "><a href="{{route('frontend.user.my_service.index')}}">My Service</a></li>
+            <li class="breadcrumb-item"><a href="{{route('frontend.user.my_service.open_service',[$service_details->service_id,$service_details->token])}}">{{$service_details->service_name}}</a></li>
+            <li class="breadcrumb-item active"><a href="{{route('frontend.user.api_builder.dashboard',[$get_app_details->ab_id,$get_app_details->key,auth()->user()->id,$service_details->service_id])}}">{{$get_app_details->app_name}}</a></li>
+        </ol>
+    </nav>
+    <!-- /Breadcrumb -->
+
+    <!-- Container -->
+
+        @include('frontend.user.service_pages.APIBuilder.pages.components.lock_main_tab_menu')
+
+        @include('frontend.user.service_pages.APIBuilder.dialogs.create_data_field')
+
+        <div class="tab-content">
+            <nav class="hk-breadcrumb" aria-label="breadcrumb">
+                <ol class="breadcrumb breadcrumb-light bg-transparent">
+                    <li class="breadcrumb-item active"><a href="{{route('frontend.user.api_builder.dashboard',[$get_app_details->ab_id,$get_app_details->key,auth()->user()->id,$service_details->service_id])}}?#builder_table">{{$get_app_details->app_name}}</a></li>
+                    <li class="breadcrumb-item active"><a href="">{{$table_details->table_name}} (Edit Table)</a></li>
+                </ol>
+            </nav>
+            <button type="button" data-toggle="modal" data-target="#insert_data_field"  class="btn btn-primary btn-rounded">Add Field</button> <br><br>
+
+            <div id="builder_table" class="tab-pane active">
+                <section class="hk-sec-wrapper">
+                    <div class="row">
+                        <div class="col-sm">
+                            <div class="table-wrap">
+                                <div class="table-responsive">
+                                    <table class="table table-sm mb-0">
+                                        <thead>
+                                        <tr>
+                                            <th>Field Name</th>
+                                            <th>Data Type</th>
+                                            <th>Key</th>
+                                            <th>Action</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>Lunar probe project</td>
+                                                <td>Text</td>
+                                                <td>KSLESDFSDFKSDF87983279</td>
+                                                <td>
+                                                    <a href="#" class="mr-25" data-toggle="tooltip" data-original-title="Edit"> <i class="icon-pencil"></i> </a>
+                                                    <a href="#" data-toggle="tooltip" data-original-title="Close"> <i class="icon-trash txt-danger"></i> </a>
+                                                </td>
+                                            </tr>
+
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    @include('frontend.user.dashboard_components.footer')
+
+    <script>
+        function location_changer(hash_code) {
+            location.hash = hash_code;
+        }
+
+    </script>
+
+    {{--<script>--}}
+        {{--$(function(){--}}
+            {{--var hash = window.location.hash;--}}
+            {{--if(hash == '#builder_table'){--}}
+                {{--$('.nav-tabs a[href="#builder_table"]').tab('show')--}}
+            {{--}else {--}}
+                {{--$('.nav-tabs a[href="#builder_dashboard"]').tab('show')--}}
+            {{--}--}}
+        {{--});--}}
+    {{--</script>--}}
