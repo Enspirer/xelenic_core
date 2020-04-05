69a9340f2fe3b390d2384d6cdf3119976054498f
Sanjaya Harshana <sanjayaharasana@gmail.com>
2020-04-06 04:45:55 +0530

    AntyFaction APIs Serivces

--- app/Http/Controllers/Frontend/CloudServices/Services/APIManager.php
+++ app/Http/Controllers/Frontend/CloudServices/Services/APIManager.php
@@ -23,6 +23,7 @@ class APIManager extends Controller
     {
         $get_published_app = CloudAPIBuilder::get_apps('published');
         $get_unplushed_app = CloudAPIBuilder::get_apps('unpublished');
+
         $tanle = [
             'get_published_app' => $get_published_app,
             'get_unpublished_app' => $get_unplushed_app,
@@ -60,6 +61,8 @@ class APIManager extends Controller
         $service_details =  MyService::get_service_details($service_id);
         $get_app_details = CloudAPIBuilder::get_app_details_by_id($app_id);
         $get_cloud_table = CloudAPIDataTable::get_tables_by_user(auth()->user()->id,$app_id);
+        $authfaction = MyService::get_service_details_by_user(2);
+
         if ($get_app_details->app_type == 0)
         {
             return redirect()->route('frontend.user.api_builder.view_app_page',[$app_id,$api_key,$user_id,$service_id]);
@@ -68,7 +71,8 @@ class APIManager extends Controller
                 [
                     'service_details' => $service_details,
                     'get_app_details' => $get_app_details,
-                    'get_cloud_table' => $get_cloud_table
+                    'get_cloud_table' => $get_cloud_table,
+                    'authfaction' => $authfaction
                 ]);
         }
     }
