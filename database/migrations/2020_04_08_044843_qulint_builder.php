6f8e6ed6f53bc6dd4596cc66c28b878ad5cf1690
Sanjaya Harshana <sanjayaharasana@gmail.com>
2020-04-08 10:38:42 +0530

    Quline Table Stucture, Page Creator Dialogs, Index. Modification

---
+++ database/migrations/2020_04_08_044843_qulint_builder.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class QulintBuilder extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('qulint_pages', function (Blueprint $table) {
+            $table->bigIncrements('q_id');
+            $table->text('title');
+            $table->integer('user_id');
+            $table->integer('service_id');
+            $table->text('type');
+            $table->string('status');
+            $table->text('custom_script_up')->nullable();
+            $table->text('custom_script_down')->nullable();
+            $table->longText('body')->nullable();
+            $table->timestamp('updated_at');
+            $table->timestamp('created_at')->useCurrent();
+        });
+
+        Schema::create('scripts', function (Blueprint $table) {
+            $table->bigIncrements('script_id');
+            $table->integer('user_id');
+            $table->integer('qulint_id');
+            $table->integer('file_id')->nullable();
+            $table->integer('service_id');
+            $table->integer('cdn')->nullable();
+            $table->string('position');
+            $table->timestamp('updated_at');
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('qulint_pages');
+        Schema::dropIfExists('scripts');
+    }
+}
--- resources/views/frontend/user/service_pages/QulintBuilder/index.blade.php
+++ resources/views/frontend/user/service_pages/QulintBuilder/index.blade.php
@@ -1,3 +1,40 @@
 @if ($service_details->service_id == 3)
-<h2>fff</h2>
+    @include('frontend.user.service_pages.QulintBuilder.dialogs.page_creator')
+
+    <div class="bg-white shadow-bottom">
+        <div class="container-fluid px-xxl-65 px-xl-20">
+            <ul class="nav nav-light nav-tabs" role="tablist">
+                <li class="nav-item active">
+                    <a data-toggle="tab" href="#menu1" class="d-flex h-60p align-items-center nav-link active">Published</a>
+                </li>
+                <li class="nav-item">
+                    <a data-toggle="tab" href="#menu2" class="d-flex h-60p align-items-center nav-link">Draft</a>
+                </li>
+            </ul>
+        </div>
+    </div> <br><br>
+
+    <div class="tab-content">
+        <div id="menu1" class="tab-pane fade in active show">
+            <div class="row">
+                <div class="col-md-3">
+                    <div style="border-radius: 5px;height: 220px;border-width: 1px;border-style: dashed;padding: 10px;border-color: #c8c8c8;text-align: center;" data-toggle="modal" data-target=".bd-example-modal-lg">
+                        <i class="fa fa-plus" style="font-size: 45px;padding-top: 30%;cursor: hand;"></i>
+                        <p>Create a APP</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+
+        <div id="menu2" class="tab-pane fade in active">
+            <div class="row">
+
+            </div>
+        </div>
+
+    </div>
+
+
+
 @endif
