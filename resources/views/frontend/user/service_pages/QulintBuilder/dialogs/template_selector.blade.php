d6b0b4e1405bf4c6ced6e27664693211eeac7d1c
Sanjaya Harshana <sanjayaharasana@gmail.com>
2020-04-09 20:44:25 +0530

    Template Selector added some Modifcatio Dialog

---
+++ resources/views/frontend/user/service_pages/QulintBuilder/dialogs/template_selector.blade.php
@@ -0,0 +1,22 @@
+<div id="filemanager">
+    <div class="header">
+        <a href="#" class="text-secondary">Pages</a>
+
+        <div class="btn-group responsive-btns mr-4 float-right" role="group">
+            <button class="btn btn-link btn-sm" title="New file" id="new-file-btn" data-vvveb-action="newPage" data-vvveb-shortcut="">
+                <i class="la la-file"></i> <small>New page</small>
+            </button>
+
+            <!--  &ensp;
+            <button class="btn btn-link text-dark p-0"  title="Delete file" id="delete-file-btn" data-vvveb-action="deletePage" data-vvveb-shortcut="">
+                <i class="la la-trash"></i> <small>Delete</small>
+            </button> -->
+        </div>
+
+    </div>
+
+    <div class="tree">
+        <ol>
+        </ol>
+    </div>
+</div>
