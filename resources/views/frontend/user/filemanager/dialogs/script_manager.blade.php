a9587a190922b123430050a5f400b45d2bec5d07
Sanjaya Harshana <sanjayaharasana@gmail.com>
2020-04-27 02:12:41 +0530

    Script Manager Script viewe

---
+++ resources/views/frontend/user/filemanager/dialogs/script_manager.blade.php
@@ -0,0 +1,30 @@
+
+<div id="script_manager" class="modal fade bd-example-modal-lg" data-backdrop="static" data-keyboard="false" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                Script manager
+            </div>
+
+            <div class="modal-body">
+                <div  class="row">
+                    <div class="col-lg">
+                        <ul class="nav nav-light nav-tabs" role="tablist">
+                            <li class="nav-item active">
+                                <a data-toggle="tab" href="#menu1" class="d-flex h-60p align-items-center nav-link active">Files</a>
+                            </li>
+                            <li class="nav-item">
+                                <a data-toggle="tab" href="#menu2" class="d-flex h-60p align-items-center nav-link">Upload</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <a href="" class="btn btn-primary pull-right">Add File</a>
+            </div>
+        </div>
+    </div>
+</div>
+
+
