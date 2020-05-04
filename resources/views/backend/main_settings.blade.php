7b720b74612bff289a3b2ca172dad20c8a536cbc
Sanjaya Harshana <sanjayaharasana@gmail.com>
2020-05-04 08:17:35 +0530

    Main Settings

--- resources/views/backend/main_settings.blade.php
+++ resources/views/backend/main_settings.blade.php
@@ -48,29 +48,6 @@
                             <label for="permission-1">Banner Section Enabled</label>
                         </div>
                     </div>
-                    <div class="group" style="background: #f4f4f4;padding: 10px;border-radius: 5px; margin-bottom: 30px;">
-                        <div class="form-group">
-                            <label>Banner Title</label>
-                            <input type="text" class="form-control" value="{{xelenic_settings('homepage_banner_head')}}" required name="homepage_banner_head">
-                        </div>
-                        <div class="form-group">
-                            <label>Banner Description</label>
-                            <textarea type="text" class="form-control" required name="homepage_banner_text" style="margin-top: 0px; margin-bottom: 0px; height: 127px;">{{xelenic_settings('homepage_banner_text')}}</textarea>
-                        </div>
-
-                        <div class="form-group">
-                            <label>Logo</label>
-                            <input type="file" class="form-control-file" style="background: white;padding: 10px;border-style: dashed;border-width: 1px;border-color: #cdcdcd;">
-                        </div>
-
-                        <div class="checkbox d-flex align-items-center">
-                            <label class="switch switch-label switch-pill switch-primary mr-2" for="permission-1">
-                                <input class="switch-input" type="checkbox" name="banner_section_status" id="permission-1" value="view backend">
-                                <span class="switch-slider" data-checked="on" data-unchecked="off"></span>
-                            </label>
-                            <label for="permission-1">Banner Section Enabled</label>
-                        </div>
-                    </div>
                 </div>
 
 
