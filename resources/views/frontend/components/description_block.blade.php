a2f3453d1c5912bd46e6e6c2461f6ec5c3a326c3
Sanjaya Harshana <sanjayaharasana@gmail.com>
2020-05-06 02:04:14 +0530

    descriptio block Blade

---
+++ resources/views/frontend/components/description_block.blade.php
@@ -0,0 +1,19 @@
+<!-- Start about Area -->
+<section class="about-area" id="about">
+    <div class="container-fluid">
+        <div class="row d-flex justify-content-end align-items-center">
+            <div class="col-lg-6 col-md-12 about-left">
+                <h1>We Believe that
+                    Interior beautifies the</h1>
+                <p>
+                    inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that, as women, our behavior on the job is beyond reproach. inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that.
+                </p>
+                <a href="#" class="primary-btn header-btn text-uppercase">See Details</a>
+            </div>
+            <div class="col-lg-6 col-md-12 about-right no-padding">
+                <img class="img-fluid" src="FrontPage/img/c1.jpg" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End about Area -->
--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -22,30 +22,19 @@
 @endif
 
 
+@if(xelenic_settings('homepage_partner_logo_section') == 'true')
+    @include('frontend.components.partner_logo')
+@elseif(xelenic_settings('homepage_partner_logo_section') == 'false')
+
+@else
+
+@endif
 
 
 
 
 
-<!-- Start about Area -->
-<section class="about-area" id="about">
-    <div class="container-fluid">
-        <div class="row d-flex justify-content-end align-items-center">
-            <div class="col-lg-6 col-md-12 about-left">
-                <h1>We Believe that
-                    Interior beautifies the</h1>
-                <p>
-                    inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that, as women, our behavior on the job is beyond reproach. inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that.
-                </p>
-                <a href="#" class="primary-btn header-btn text-uppercase">See Details</a>
-            </div>
-            <div class="col-lg-6 col-md-12 about-right no-padding">
-                <img class="img-fluid" src="FrontPage/img/c1.jpg" alt="">
-            </div>
-        </div>
-    </div>
-</section>
-<!-- End about Area -->
+
 
 <!-- Start service Area -->
 <section class="service-area section-gap" id="service">
