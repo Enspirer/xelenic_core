ec6d3d171b958344f00376319d4579cffc5423e5
Sanjaya Harshana <sanjayaharasana@gmail.com>
2020-05-06 13:09:37 +0530

    Feature Data Blades

--- resources/views/backend/settings/settings_block/feature_data.blade.php
+++ resources/views/backend/settings/settings_block/feature_data.blade.php
@@ -1,13 +1,32 @@
 @foreach(xelenic_settings('homepage_feature_data') as $feature_data)
-    <div style="border-style: dashed;background: #ebebeb;border-width: 1px;padding: 10px;margin-bottom: 10px;/* color: white; */">
-        <div class="form-group">
-            <label>Title</label>
-            <input type="text" class="form-control" name="head[]" style="font-size: 11px;padding-top: 0px;padding-bottom: 0px;height: 27px;">
-        </div>
-
-        <div class="form-group">
-            <label>Description</label>
-            <textarea type="text" class="form-control" name="descript[]" style="font-size: 11px;padding-top: 0px;padding-bottom: 0px;height: 77px;"></textarea>
+    <div style="background: #ebebeb;border-width: 1px;padding: 10px;margin-bottom: 10px;/* color: white; */">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="form-group">
+                    <label>Title</label>
+                    <input type="text" class="form-control" name="head[]" style="" value="{{$feature_data->head}}">
+                </div>
+                <div class="form-group">
+                    <label>Description</label>
+                    <textarea type="text" class="form-control" name="description[]" style="height: 77px;">{{$feature_data->description}}</textarea>
+                </div>
+            </div>
+            <div class="col-md-6">
+               <div class="form-group">
+                   <label>link</label>
+                   <input type="text" class="form-control" name="link[]">
+               </div>
+                <div class="form-group">
+                    <label>Feature Icon</label>
+                    <select class="form-control" name="icon[]">
+                        <option value="fa fa-icon">icon fa-book</option>
+                        <option value="fa fa-icon">icon fa-book</option>
+                        <option value="fa fa-icon">icon fa-book</option>
+                        <option value="fa fa-icon">icon fa-book</option>
+                        <option value="fa fa-icon">icon fa-book</option>
+                    </select>
+                </div>
+            </div>
         </div>
     </div>
 @endforeach
