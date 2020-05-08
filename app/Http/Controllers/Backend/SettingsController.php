f5a41c22fc2443c270f57eff7b44ecab47e63f41
Sanjaya Harshana <sanjayaharasana@gmail.com>
2020-05-08 07:53:26 +0530

    Settings Controller

--- app/Http/Controllers/Backend/SettingsController.php
+++ app/Http/Controllers/Backend/SettingsController.php
@@ -15,8 +15,40 @@ class SettingsController extends Controller
 
     public function store(Request $request)
     {
-//        Settings::set_settings('','');
 
-        dd($request);
+        $banner_section = self::enabled_state($request->banner_section);
+        $homepage_partner_logo_section = self::enabled_state($request->homepage_partner_logo_section);
+        $homepage_block_1 = self::enabled_state($request->homepage_block_1);
+
+        Settings::set_settings('banner_section', $banner_section);
+        Settings::set_settings('homepage_block_1', $homepage_block_1);
+        Settings::set_settings('homepage_partner_logo_section', $homepage_partner_logo_section);
+
+
+
+
+        Settings::set_settings('homepage_banner_head',$request->homepage_banner_head);
+        Settings::set_settings('homepage_banner_text',$request->homepage_banner_text);
+
+
+        Settings::set_settings('homepage_partner_logo_section_title', $request->homepage_partner_logo_section_title);
+
+      return back();
+    }
+
+
+    public static function enabled_state ($string)
+    {
+        if ($string == null)
+        {
+            $output = 0;
+        }else if ($string == 'on' ){
+            $output = 1;
+        }else
+        {
+            $output = 0;
+        }
+
+        return $output;
     }
 }
