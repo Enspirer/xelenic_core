b1bd6fe99062f34a95afa8548157e9b7fb67f689
Amanda Balarica <amandabalarica@gmail.com>
2020-05-09 02:23:45 +0530

    Feature Data Blade

--- resources/views/backend/settings/settings_block/feature_data.blade.php
+++ resources/views/backend/settings/settings_block/feature_data.blade.php
@@ -1,40 +1,90 @@
 
 <form action="{{route('admin.settings.home_page.save_features_data.save')}}" method="post">
     {{csrf_field()}}
-    @foreach(xelenic_settings('homepage_feature_data') as $feature_data)
-        <div style="background: #ebebeb;border-width: 1px;padding: 10px;margin-bottom: 10px;/* color: white; */">
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label>Title</label>
-                        <input type="text" class="form-control" name="head[]" style="" value="{{$feature_data->head}}">
-                        <input type="hidden" class="form-control" name="id[]" style="" value="1">
+    <div id="vole">
+        @foreach(xelenic_settings('homepage_feature_data') as $feature_data)
+            <div style="background: #ebebeb;border-width: 1px;padding: 10px;margin-bottom: 10px;/* color: white; */" id="{{$feature_data->id}}">
+                <div class="row">
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label>Title</label>
+                            <input type="text" class="form-control" name="head[]" style="" value="{{$feature_data->head}}">
+                            <input type="hidden" class="form-control" name="id[]" style="" value="1">
+                        </div>
+                        <div class="form-group">
+                            <label>Description</label>
+                            <textarea type="text" class="form-control" name="description[]" style="height: 77px;">{{$feature_data->description}}</textarea>
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label>Description</label>
-                        <textarea type="text" class="form-control" name="description[]" style="height: 77px;">{{$feature_data->description}}</textarea>
+                    <div class="col-md-6">
+                        <div class="form-group">
+                            <label>link</label>
+                            <input type="text" class="form-control" name="link[]">
+                        </div>
+                        <div class="form-group">
+                            <label>Feature Icon</label>
+                            <select class="form-control" name="icon[]">
+                                <option value="fa fa-icon">icon fa-book</option>
+                                <option value="fa fa-icon">icon fa-book</option>
+                                <option value="fa fa-icon">icon fa-book</option>
+                                <option value="fa fa-icon">icon fa-book</option>
+                                <option value="fa fa-icon">icon fa-book</option>
+                            </select>
+                        </div>
                     </div>
                 </div>
-                <div class="col-md-6">
-                    <div class="form-group">
-                        <label>link</label>
-                        <input type="text" class="form-control" name="link[]">
-                    </div>
-                    <div class="form-group">
-                        <label>Feature Icon</label>
-                        <select class="form-control" name="icon[]">
-                            <option value="fa fa-icon">icon fa-book</option>
-                            <option value="fa fa-icon">icon fa-book</option>
-                            <option value="fa fa-icon">icon fa-book</option>
-                            <option value="fa fa-icon">icon fa-book</option>
-                            <option value="fa fa-icon">icon fa-book</option>
-                        </select>
-                    </div>
-                </div>
+                <div class="btn btn-danger" onclick="delete_delemt({{$feature_data->id}})"><i class="fa fa-trash"></i></div>
             </div>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
+
+    <div class="btn btn-danger" onclick="add_element('11')"><i class="fa fa-trash"></i></div>
 
     <button class="btn btn-primary" type="submit">Save Settings</button>
 
 </form>
+
+<script>
+    function delete_delemt(id) {
+        var elem = document.getElementById(id);
+        return elem.parentNode.removeChild(elem);
+    }
+
+    function add_element($id) {
+        $("#vole").append('<div style="background: #ebebeb;border-width: 1px;padding: 10px;margin-bottom: 10px;/* color: white; */" id="{{$feature_data->id}}">' +
+        '<div class="row">' +
+            '<div class="col-md-6">' +
+                    '<div class="form-group">'+
+                        '<label>Title</label>' +
+                        '<input type="text" class="form-control" name="head[]" style="" value="{{$feature_data->head}}">' +
+                        '<input type="hidden" class="form-control" name="id[]" style="" value="1">' +
+                    '</div>'+
+                    '<div class="form-group">' +
+                        '<label>Description</label>' +
+                        '<textarea type="text" class="form-control" name="description[]" style="height: 77px;">{{$feature_data->description}}</textarea>' +
+                    '</div>' +
+                '</div>' +
+            '<div class="col-md-6">' +
+            '<div class="form-group">' +
+            '<label>link</label>' +
+            '<input type="text" class="form-control" name="link[]">' +
+            '</div>' +
+            '<div class="form-group">' +
+            '<label>Feature Icon</label>' +
+            '<select class="form-control" name="icon[]">' +
+            '<option value="fa fa-icon">icon fa-book</option>' +
+        '<option value="fa fa-icon">icon fa-book</option>'+
+        '<option value="fa fa-icon">icon fa-book</option>' +
+        '<option value="fa fa-icon">icon fa-book</option>' +
+        '<option value="fa fa-icon">icon fa-book</option>' +
+        '</select>' +
+        '</div>' +
+        '</div>' +
+            '<div class="btn btn-danger" onclick="delete_delemt({{$feature_data->id}})"><i class="fa fa-trash"></i></div>' +
+                '</div>' +
+                '</div>' +
+            '</div>');
+
+
+    }
+</script>
