c1301c45c135cca488dbe2be40bf47b8d9c1fca1
Amanda Balarica <amandabalarica@gmail.com>
2020-05-08 15:59:20 +0530

    Descrtipn Block

--- resources/views/frontend/components/description_block.blade.php
+++ resources/views/frontend/components/description_block.blade.php
@@ -3,11 +3,8 @@
     <div class="container-fluid">
         <div class="row d-flex justify-content-end align-items-center">
             <div class="col-lg-6 col-md-12 about-left">
-                <h1>We Believe that
-                    Interior beautifies the</h1>
-                <p>
-                    inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that, as women, our behavior on the job is beyond reproach. inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards especially in the workplace. That’s why it’s crucial that.
-                </p>
+                <h1>{{xelenic_settings('homepage_desc_block_1_title')}}</h1>
+                <p>{{xelenic_settings('homepage_desc_block_1_desc')}}</p>
                 <a href="#" class="primary-btn header-btn text-uppercase">See Details</a>
             </div>
             <div class="col-lg-6 col-md-12 about-right no-padding">
