ecadcd86633c1f3195d60138a1032c703f190670
Amanda Balarica <amandabalarica@gmail.com>
2020-05-09 18:16:17 +0530

    Dashboard Widgets

--- resources/views/frontend/user/dashboard.blade.php
+++ resources/views/frontend/user/dashboard.blade.php
@@ -30,21 +30,26 @@
         <div class="container-fluid px-xxl-65 px-xl-20">
 
             <div class="row">
-                <div class="col-xl-4">
-                    <div class="card card-lg">
+                <div class="col-xl-12">
+                    <div class="card card-lg" style="background: #f9f9f9;">
                         <div class="card-body">
-                            <h4>{{ xelenic_settings('appple') }}</h4> <br>
-                            <p>You received this message because you are subscribed to the Google Groups "Slax users" group.
-                                To unsubscribe from this group and stop receiving emails from it, send an email to
-                                slax-users+unsubscribe@googlegroups.com. To view this discussion on the web
-                                visit https://groups.google.com/d/msgid/slax-users
-                                /7018382b-f451-4a2f-8695-eba4874efc11%40googlegroups.com
-                            </p>
-
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <div class="" style="background: url('{{url('/')}}/images/create_website.jpg');height: 250px;background-size: contain;background-repeat: no-repeat;background-position: center;"></div>
+                                </div>
+                                <div class="col-md-8" style="text-align: left;">
+                                    <h3>Welcome to Thingerbits</h3><br>
+                                    <p style="font-size: 18px;">
+                                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been
+                                        the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley
+                                        of type and scrambled it to make a type specimen book. It has survived not only five centuries,
+                                        but also the leap into electronic typesetting,
+                                    </p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
-
             </div>
 
         </div>
