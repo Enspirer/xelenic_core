eed821bfcdc2ace5d0e8de863f4f86a7684f4ac0
Amanda Balarica <amandabalarica@gmail.com>
2020-05-10 11:20:45 +0530

    Image Banner

---
+++ resources/views/frontend/components/image_banner.blade.php
@@ -0,0 +1,20 @@
+
+
+<!-- Start info Area -->
+<section class="info-area relative section-gap">
+    <div class="overlay overlay-bg"></div>
+    <div class="container">
+        <div class="row justify-content-start">
+            <div class="col-lg-5">
+                <h1 class="text-white">From the root <br>
+                    of experience We dig <br>
+                    out the best talent</h1>
+                <p class="pt-20 pb-20">
+                    inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards – especially in the workplace. That’s why it’s crucial that, as women.
+                </p>
+                <a href="#" class="primary-btn header-btn text-uppercase">Get Customer Support</a>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- End info Area -->
