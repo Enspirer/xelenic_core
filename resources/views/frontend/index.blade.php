02ccc109317e08171b96bbafacaafd119bcdded2
Amanda Balarica <amandabalarica@gmail.com>
2020-05-10 11:20:55 +0530

    Index Infor

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -47,29 +47,19 @@
 
 
 
+@if(xelenic_settings('homepage_custom_solution_section') == '1')
+    @include('frontend.components.custom_solution')
+@elseif(xelenic_settings('homepage_custom_solution_section') == '0')
 
-<!-- Start info Area -->
-<section class="info-area relative section-gap">
-    <div class="overlay overlay-bg"></div>
-    <div class="container">
-        <div class="row justify-content-start">
-            <div class="col-lg-5">
-                <h1 class="text-white">From the root <br>
-                    of experience We dig <br>
-                    out the best talent</h1>
-                <p class="pt-20 pb-20">
-                    inappropriate behavior is often laughed off as “boys will be boys,” women face higher conduct standards – especially in the workplace. That’s why it’s crucial that, as women.
-                </p>
-                <a href="#" class="primary-btn header-btn text-uppercase">Get Customer Support</a>
-            </div>
-        </div>
-    </div>
-</section>
-<!-- End info Area -->
+@else
+
+@endif
+
+
 
 
 <!-- Start faq Area -->
-<section class="faq-area section-gap" id="faq">
+<section class="faq-area section-gap" id="faq" style="">
     <div class="container">
         <div class="row d-flex justify-content-center">
             <div class="menu-content pb-60 col-lg-8">
@@ -129,45 +119,9 @@
 </section>
 <!-- End faq Area -->
 
-@if(xelenic_settings('homepage_custom_solution_section') == 'true')
-    @include('frontend.components.custom_solution')
-@elseif(xelenic_settings('homepage_custom_solution_section') == 'true')
-
-@else
-
-@endif
-
 
 <!-- start contact Area -->
-<section class="contact-area section-gap" id="contact">
-    <div class="container">
-        <div class="row d-flex justify-content-center">
-            <div class="menu-content pb-30 col-lg-8">
-                <div class="title text-center">
-                    <h1 class="mb-10">Contact Us</h1>
-                    <p>Who are in extremely love with eco friendly system.</p>
-                </div>
-            </div>
-        </div>
-        <form class="form-area mt-60" id="myForm" action="mail.php" method="post" class="contact-form text-right">
-            <div class="row">
-                <div class="col-lg-6 form-group">
-                    <input name="name" placeholder="Enter your name" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your name'" class="common-input mb-20 form-control" required="" type="text">
 
-                    <input name="email" placeholder="Enter email address" pattern="[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter email address'" class="common-input mb-20 form-control" required="" type="email">
-
-                    <input name="subject" placeholder="Enter your subject" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Enter your subject'" class="common-input mb-20 form-control" required="" type="text">
-                </div>
-                <div class="col-lg-6 form-group">
-                    <textarea class="common-textarea mt-10 form-control" name="message" placeholder="Messege" onfocus="this.placeholder = ''" onblur="this.placeholder = 'Messege'" required=""></textarea>
-                    <button class="primary-btn mt-20">Send Message<span class="lnr lnr-arrow-right"></span></button>
-                    <div class="mt-10 alert-msg">
-                    </div>
-                </div></div>
-        </form>
-
-    </div>
-</section>
 <!-- end contact Area -->
 
 
