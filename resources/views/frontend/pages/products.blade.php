14d94d9e16ddf035bd824fe4edc36c16f643ee27
Amanda Balarica <amandabalarica@gmail.com>
2020-05-11 11:03:53 +0530

    Product blades

--- resources/views/frontend/pages/products.blade.php
+++ resources/views/frontend/pages/products.blade.php
@@ -1,1 +1,25 @@
-Products
+
+@include('frontend.components.head')
+<body>
+@include('frontend.components.header')
+
+
+
+@include('frontend.includes.wave')
+
+
+
+<div class="row">
+    <div class="col-md-6">
+        
+    </div>
+    <div class="col-md-6">
+
+    </div>
+</div>
+
+
+@include('frontend.components.footer')
+@include('frontend.components.scripts')
+</body>
+</html>
