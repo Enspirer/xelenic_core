df500dc29c610e97cc9c50d2e0912d84bbf5d8d1
Amanda Balarica <amandabalarica@gmail.com>
2020-05-11 12:12:35 +0530

    Product Blade Added Changers More

--- resources/views/frontend/pages/products.blade.php
+++ resources/views/frontend/pages/products.blade.php
@@ -5,17 +5,44 @@
 
 
 
-@include('frontend.includes.wave')
+<br><br><br><br>
 
-
-
-<div class="row">
-    <div class="col-md-6">
-        
+<div class="row d-flex justify-content-center">
+    <div class="menu-content pb-60 col-lg-8">
+        <div class="title text-center">
+            <h1 class="mb-10">Features That make us Unique</h1>
+            <p>Who are in extremely love with eco friendly system.</p>
+        </div>
     </div>
-    <div class="col-md-6">
+</div>
 
+<div class="container">
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card" style="height: 300px;">
+
+                <p style="text-align: center;">AnimTrap CSS</p>
+
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card" style="height: 300px;">
+
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card" style="height: 300px;">
+
+            </div>
+        </div>
+        <div class="col-md-3">
+            <div class="card" style="height: 300px;">
+
+            </div>
+        </div>
     </div>
+    <br><br>
+
 </div>
 
 
