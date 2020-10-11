2e1f8fe3368c0be91f518416f8115585b1a8558d
Amanda Balarica <amandabalarica@gmail.com>
2020-10-11 22:38:55 +0530

    Converted API Builder PHP

--- app/ServiceModel/CloudAPIBuilder/CloudAPIBuilder.php
+++ app/ServiceModel/CloudAPIBuilder/CloudAPIBuilder.php
@@ -2,6 +2,7 @@
 
 namespace App\ServiceModel\CloudAPIBuilder;
 
+use App\MyService;
 use Illuminate\Database\Eloquent\Model;
 use DB;
 
@@ -13,37 +14,32 @@ class CloudAPIBuilder extends Model
     public static function create_app($app_name,$app_type,$status,$key)
     {
 
-        $get_app_id = DB::table('cloud_api_builder')
-            ->insertGetId(
-                [
-                    'app_name' => $app_name,
-                    'user_id' => auth()->user()->id,
-                    'app_type' => $app_type,
-                    'status' => $status,
-                    'key' => $key,
-                ]
-            );
+        $cloudAPIBuilder = new CloudAPIBuilder;
+        $cloudAPIBuilder->app_name = $app_name;
+        $cloudAPIBuilder->user_id = auth()->user()->id;
+        $cloudAPIBuilder->app_type = $app_type;
+        $cloudAPIBuilder->status = $status;
+        $cloudAPIBuilder->key = $key;
+        $cloudAPIBuilder->save();
+        $get_app_id = $cloudAPIBuilder->ab_id;
 
-        $get_all_services = DB::table('cloud_api_builder')
-            ->where('user_id', auth()->user()->id)
+        $get_all_services = CloudAPIBuilder::where('user_id', auth()->user()->id)
             ->get();
+
         $get_service_count = count($get_all_services);
 
-        DB::table('my_service')
-            ->where('service_id',1 )
-            ->where('user_id', auth()->user()->id)
+         MyService::where('service_id',1)
+            ->where('user_id',auth()->user()->id)
             ->update(
                 ['intence_count' => $get_service_count]
             );
-
         return $get_app_id ;
 
     }
 
     public static function change_app_type ($ab_id,$app_type)
     {
-        DB::table('cloud_api_builder')
-            ->where('ab_id',$ab_id)
+        CloudAPIBuilder::where('ab_id',$ab_id)
             ->update(
                 ['app_type' => $app_type]
             );
@@ -52,8 +48,7 @@ class CloudAPIBuilder extends Model
 
     public static function update_app ($app_id,$app_name,$app_type,$status,$key)
     {
-        DB::table('cloud_api_builder')
-            ->where('ab_id', $app_id)
+        CloudAPIBuilder::where('ab_id', $app_id)
             ->update(
                 [
                     'app_name' => $app_name,
@@ -68,8 +63,7 @@ class CloudAPIBuilder extends Model
 
     public static function get_apps ($status)
     {
-        $get_apps =DB::table('cloud_api_builder')
-                    ->where('status',$status)
+        $get_apps =CloudAPIBuilder::where('status',$status)
                     ->where('user_id',auth()->user()->id)
                     ->get();
 
@@ -78,15 +72,13 @@ class CloudAPIBuilder extends Model
 
     public static function get_next_app_order_id()
     {
-        $get_apps =DB::table('cloud_api_builder')
-            ->where('user_id',auth()->user()->id)
+        CloudAPIBuilder::where('user_id',auth()->user()->id)
             ->get();
     }
 
     public static function get_app_details_by_id ($id)
     {
-        $get_app_details = DB::table('cloud_api_builder')
-            ->where('ab_id',$id)
+        $get_app_details = CloudAPIBuilder::where('ab_id',$id)
             ->where('user_id',auth()->user()->id)
             ->first();
         return $get_app_details;
