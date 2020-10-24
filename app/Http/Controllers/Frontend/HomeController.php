1facdd93176a436a334abba9d511631e4da38933
Sanjaya Senevirathne <sanjayaharasana@gmail.com>
2020-10-25 03:56:24 +0530

    Home Controller FeedBack Servicer Function Create

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -16,4 +16,9 @@ class HomeController extends Controller
     {
         return view('frontend.index');
     }
+
+    public function service_error($error_module)
+    {
+        return view('frontend.pages.service_error');
+    }
 }
